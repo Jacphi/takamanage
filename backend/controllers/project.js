734f296890007505232344e8a9c7f38badcb01ff
Jacphi NOUKPO <jnoukpo@gmail.com>
2024-09-18 05:53:07 +0100

    Create project - retrieve single project information logic added in project controller

---
+++ backend/controllers/project.js
@@ -0,0 +1,72 @@
+const Project = require("../models/project");
+
+const User = require("../models/user");
+
+const Task = require("../models/task");
+
+exports.addProject = (req, res, next) => {
+  const name = req.body.name;
+  const description = req.body.description;
+  const status = "On track";
+  const chat = "chat";
+  let admin;
+
+  const project = new Project({
+    name: name,
+    description: description,
+    admin: req.userId,
+    status: status,
+    chat: chat,
+  });
+
+  project
+    .save()
+    .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      admin = user;
+      user.projects.push(project);
+      return user.save();
+    })
+    .then((result) => {
+      console.log("Created project");
+      res.status(201).json({
+        message: "Project created",
+        project: project,
+        admin: { _id: admin._id, name: admin.email },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      const error = new Error("Create project failed");
+      error.statusCode = 422;
+      next(err);
+    });
+};
+
+//exports.getProjects = (req, res, next) => {
+//  Project.find()
+//    .populate("admin")
+//    .then((projects) => {
+//      res.json(projects);
+//      console.log(projects);
+//    });
+//};
+
+exports.getProject = (req, res, next) => {
+  const projectId = req.params.id;
+  Project.findById(projectId)
+    .then((project) => {
+      console.log("Project");
+      res.status(200).json({
+        project: project,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
