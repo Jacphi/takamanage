0d5522509b9027e185325dbbc1fe627289122c40
Jacphi NOUKPO <jnoukpo@gmail.com>
2024-09-18 05:55:51 +0100

    Get project members - Check user admin logic added in project controller

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -70,3 +70,49 @@ exports.getProject = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getMembers = (req, res, next) => {
+  const projectId = req.params.id;
+
+  User.find({ projects: { $elemMatch: { $eq: projectId } } }, "-password")
+    .then((users) => {
+      console.log("Project members");
+      res.status(200).json({
+        message: "Project members",
+        members: users,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.isAdmin = (req, res, next) => {
+  const projectId = req.body.project;
+
+  Project.findById(projectId)
+    .then((project) => {
+      if (req.userId !== project.admin.toString()) {
+        console.log("Not admin");
+        res.status(200).json({
+          message: "Not admin",
+          data: false,
+        });
+      } else {
+        console.log("Admin");
+        res.status(200).json({
+          message: "Admin",
+          data: true,
+        });
+      }
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
